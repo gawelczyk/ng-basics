b8d0f51d85e57d830b607d440acd18f44059ecc2
Adam Gawelczyk <adam.gawelczyk@motorolasolutions.com>
2014-07-18 14:31:31 +0200

    PeopleListCtrl implemented, move up/down should be refactored

--- js/controllers/PeopleListCtrl.js
+++ js/controllers/PeopleListCtrl.js
@@ -1,15 +1,46 @@
 MyApp.controller("PeopleListCtrl", [
-  "$scope",
-  "peopleSearchSrv",
-  function($scope, peopleSearchSrv) {
-    $scope.people = peopleSearchSrv.findAll();
+    "$scope",
+    "peopleSearchSrv",
+    function ($scope, peopleSearchSrv) {
+        $scope.people = peopleSearchSrv.findAll();
 
-    $scope.greetWith = function(person) {
-      log("Hi " + person.name + "!");
-    };
+        $scope.newPerson = {name: '', age: ''};
 
-    $scope.createNew = function() {
-      $scope.people.push({name: "Bob" + Math.random()});
-    };
-  }
+        $scope.greetWith = function (person) {
+            log("Hi " + person.name + "!");
+        };
+
+        $scope.createNew = function () {
+            $scope.people.push({name: $scope.newPerson.name, age: $scope.newPerson.age});
+            $scope.newPerson = {name: '', age: ''};
+        };
+
+        $scope.clearAll = function () {
+            $scope.people = [];
+        };
+
+        var move = function (array, el, step) {
+            var index = array.indexOf(el);
+            var newIndex = index + step;
+            if (newIndex < 0) {
+                newIndex = array.length - 1;
+            }
+            if (newIndex > array.length - 1) {
+                newIndex = 0;
+            }
+            var el1 = $scope.people[index];
+            var el2 = $scope.people[newIndex];
+            $scope.people[newIndex] = el1;
+            $scope.people[index] = el2;
+        }
+
+        $scope.moveUp = function (person) {
+            move($scope.people, person, -1);
+        };
+
+        $scope.moveDown = function (person) {
+            move($scope.people, person, 1);
+        };
+
+    }
 ]);
